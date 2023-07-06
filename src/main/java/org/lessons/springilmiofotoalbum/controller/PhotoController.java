ca5d8ec58b377f8e9a77a13477b62e08cf1788f5
Diletta Pelloni <dilpel@live.it>
2023-07-06 14:56:39 +0200

    Impostata create

--- src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -1,5 +1,7 @@
 package org.lessons.springilmiofotoalbum.controller;
 
+import org.lessons.springilmiofotoalbum.dto.PhotoDto;
+import org.lessons.springilmiofotoalbum.repository.CategoryRepository;
 import org.lessons.springilmiofotoalbum.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.Optional;
 
 @Controller
@@ -16,6 +17,8 @@ public class PhotoController {
 
     @Autowired
     PhotoService photoService;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @GetMapping
     public String index(
@@ -27,6 +30,15 @@ public class PhotoController {
             model.addAttribute("keyword", keyword.get());
         }
         return "/photos/index";
+    }
+
+    @GetMapping("/create")
+    public String create(
+        Model model
+    ) {
+        model.addAttribute("catList", categoryRepository.findAll());
+        model.addAttribute("photo", new PhotoDto());
+        return "/photos/editor";
     }
 
 }
---
+++ src/main/java/org/lessons/springilmiofotoalbum/dto/PhotoDto.java
@@ -0,0 +1,58 @@
+package org.lessons.springilmiofotoalbum.dto;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.lessons.springilmiofotoalbum.model.Category;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotoDto {
+    //ATTRIBUTI ------------------------------------------------------------------------------
+    private Integer id;
+
+    @NotBlank(message = "Name must not be null or blank")
+    @Size(min=3, max=255, message = "Name must have min 3 and max 255 characters")
+    private String title;
+
+    @Size(max=1600, message = "Description must have max 1600 characters")
+    private String description;
+
+    private MultipartFile img;
+
+    private List<Category> categories = new ArrayList<>();
+
+    //GETTER E SETTER ------------------------------------------------------------------------------
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public MultipartFile getImg() {
+        return img;
+    }
+    public void setImg(MultipartFile img) {
+        this.img = img;
+    }
+    public List<Category> getCategories() {
+        return categories;
+    }
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+}
