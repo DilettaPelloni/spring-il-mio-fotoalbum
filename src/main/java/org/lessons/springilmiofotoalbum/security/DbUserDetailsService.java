6823133c23df9575bce25bc295cba9a14d2b6225
Diletta Pelloni <dilpel@live.it>
2023-07-07 09:29:03 +0200

    Fatto userDetailsService

---
+++ src/main/java/org/lessons/springilmiofotoalbum/security/DbUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.lessons.springilmiofotoalbum.security;
+
+import org.lessons.springilmiofotoalbum.model.User;
+import org.lessons.springilmiofotoalbum.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DbUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+        if (user.isEmpty()){
+            throw new UsernameNotFoundException("user with email "+username+" not found");
+        }
+        return new DbUserDetails(user.get());
+    }
+
+}
